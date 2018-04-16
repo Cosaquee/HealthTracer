4e352c8e85b6936adbc764285f8eac8ddbc37d96
Karol Kozakowski <cosaquee@gmail.com>
2018-04-16 18:38:54 +0200

    Initial Commit

---
+++ HealtTracer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealtTracer
+//
+//  Created by Karol Kozakowski on 16/04/2018.
+//  Copyright © 2018 Karol Kozakowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
